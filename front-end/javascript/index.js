affa4f9d3b77fd681081616eff81ed5f9fa9c4c0
Pattadon Bao-ngern <spectreb.boss@gmail.com>
2017-10-16 11:45:29 +0700

    hero section and popup for login and register

--- front-end/javascript/index.js
+++ front-end/javascript/index.js
@@ -1,2 +1,27 @@
-var text = 'hello world';
-document.getElementById('test').innerText = text;
+complete = false;
+window.onload = function () {
+  complete = true;
+  console.log('loaded complete');
+  popup.onclick = function () {
+    popup.style.display = 'none';
+    loginPopup.style.display = 'none';
+    regisPopup.style.display = 'none';
+  }
+}
+var popup = document.getElementById('popup');
+var regisPopup = document.getElementById('regis-input');
+var loginPopup = document.getElementById('login-input');
+
+function loginForm() {
+  if (complete) {
+    popup.style.display = 'flex';
+    loginPopup.style.display = 'inherit';
+  }
+}
+
+function regisForm() {
+  if (complete) {
+    popup.style.display = 'flex';
+    regisPopup.style.display = 'inherit';
+  }
+}
