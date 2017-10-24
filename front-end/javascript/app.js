71bad4578bbcfc79adaec26fe652953824f3fa31
opfine1 <o_nongearth_o@hotmail.com>
2017-10-24 02:42:55 -0700

    เพิ่ม ระบบ login กับ Signup
    ในส่วนของ logout ยังไม่ได้ทำนะจ๊ะ เพราะยังไม่เห็นหน้าตาเลย เชื่อมไม่ถูก

--- front-end/javascript/app.js
+++ front-end/javascript/app.js
@@ -1,9 +1,57 @@
-var config = {
+function(){
+
+	// Initialize Firebase
+	var config = {
     apiKey: "AIzaSyD6O4r9R45V--vk3Tuup-tH-CP8wTu0NQ4",
     authDomain: "parttime-finder.firebaseapp.com",
     databaseURL: "https://parttime-finder.firebaseio.com",
     projectId: "parttime-finder",
     storageBucket: "parttime-finder.appspot.com",
     messagingSenderId: "479332450590"
-};
-firebase.initializeApp(config);
+	};
+	firebase.initializeApp(config);
+
+	//SignIn Function
+	function getSignin(){
+		const txtEmail = document.getElementById('email');
+		const txtPassword = document.getElementById('password');
+		login.addEventListener('click', e => {
+			//get email and password
+			const email = txtEmail.value;
+			const pass = txtPassword.value;
+			const auth = firebase.auth();
+			const promise = auth.signInWithEmailAndPassword(email, pass);
+			promise.catch(e => console.log(e.message));
+		});
+		
+	}
+
+	//SignUp Function
+	function getSignup(){
+		const txtEmail = document.getElemntById('email');
+		const txtPassword = document.getElementById('password');
+		signup.addEventListener('click', e => {
+			const email = txtEmail.value;
+			const pass = txtPassword.value;
+			const auth = firebase.auth();
+			const promise = auth.createUserWithEmailAndPassword(email, pass);
+			promise.catch(e => console.log(e.message));
+		});
+	}
+
+
+	// firebase.auth().onAuthStateChanged(firebaseUser => {
+	// 	if(firebaseUser){
+	// 		console.log(firebaseUser);
+	// 	}else{
+	// 		console.log('not logged in');
+	// 	}
+	// });
+
+
+	// buttonlogout.addEventListener('click', e => {
+	// 	firebase.auth().signOut();
+	// });
+
+
+}();
