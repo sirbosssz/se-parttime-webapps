c75b963c635cfd4c13cc4d0b03dc50cdd47c276e
opfine1 <o_nongearth_o@hotmail.com>
2017-10-24 01:43:27 -0700

    เชื่อม database firebase
    ปล. ยังไม่ได้เชื่อมข้อมูลตรงส่วนของ regis กับส่วนอื่นเด้อ ศึกษาอยู่

---
+++ front-end/javascript/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyD6O4r9R45V--vk3Tuup-tH-CP8wTu0NQ4",
+    authDomain: "parttime-finder.firebaseapp.com",
+    databaseURL: "https://parttime-finder.firebaseio.com",
+    projectId: "parttime-finder",
+    storageBucket: "parttime-finder.appspot.com",
+    messagingSenderId: "479332450590"
+};
+firebase.initializeApp(config);
