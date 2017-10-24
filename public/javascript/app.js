91c1e434bebc5309fb468581251e97e72e3e4ecb
Pattadon Bao-ngern <spectreb.boss@gmail.com>
2017-10-25 02:10:40 +0700

    authentication bug fixed - login to website by email - register to website by email

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,57 +1,34 @@
-function(){
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyD6O4r9R45V--vk3Tuup-tH-CP8wTu0NQ4",
+	authDomain: "parttime-finder.firebaseapp.com",
+	databaseURL: "https://parttime-finder.firebaseio.com",
+	projectId: "parttime-finder",
+	storageBucket: "parttime-finder.appspot.com",
+	messagingSenderId: "479332450590"
+};
+firebase.initializeApp(config);
 
-	// Initialize Firebase
-	var config = {
-    apiKey: "AIzaSyD6O4r9R45V--vk3Tuup-tH-CP8wTu0NQ4",
-    authDomain: "parttime-finder.firebaseapp.com",
-    databaseURL: "https://parttime-finder.firebaseio.com",
-    projectId: "parttime-finder",
-    storageBucket: "parttime-finder.appspot.com",
-    messagingSenderId: "479332450590"
-	};
-	firebase.initializeApp(config);
+// sign in function
+function getSignIn() {
+	var email = document.forms['login-form']['email'].value;
+	var password = document.forms['login-form']['password'].value;
+	firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+		// error handling
+		var errorCode = error.code;
+		var errorMessage = error.message;
+	});
+	console.log(email + ' sign in complete');
+}
 
-	//SignIn Function
-	function getSignin(){
-		const txtEmail = document.getElementById('email');
-		const txtPassword = document.getElementById('password');
-		login.addEventListener('click', e => {
-			//get email and password
-			const email = txtEmail.value;
-			const pass = txtPassword.value;
-			const auth = firebase.auth();
-			const promise = auth.signInWithEmailAndPassword(email, pass);
-			promise.catch(e => console.log(e.message));
-		});
-		
-	}
-
-	//SignUp Function
-	function getSignup(){
-		const txtEmail = document.getElemntById('email');
-		const txtPassword = document.getElementById('password');
-		signup.addEventListener('click', e => {
-			const email = txtEmail.value;
-			const pass = txtPassword.value;
-			const auth = firebase.auth();
-			const promise = auth.createUserWithEmailAndPassword(email, pass);
-			promise.catch(e => console.log(e.message));
-		});
-	}
-
-
-	// firebase.auth().onAuthStateChanged(firebaseUser => {
-	// 	if(firebaseUser){
-	// 		console.log(firebaseUser);
-	// 	}else{
-	// 		console.log('not logged in');
-	// 	}
-	// });
-
-
-	// buttonlogout.addEventListener('click', e => {
-	// 	firebase.auth().signOut();
-	// });
-
-
-}();
+// sign up function
+function getSignUp() {
+	var email = document.forms['regis-form']['email'].value;
+	var password = document.forms['regis-form']['password'].value;
+	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+		// error handling
+		var errorCode = error.code;
+		var errorMessage = error.message;
+	});
+	console.log(email + ' sign up complete');
+}
