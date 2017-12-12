627f0e1713e57e1bf53cadc6a4f924f1bae31808
Boss'sr ShadowZ <spectreb.boss@gmail.com>
2017-12-12 20:37:20 +0700

    a little debug

--- src/components/Popup/RegisterPopup.jsx
+++ src/components/Popup/RegisterPopup.jsx
@@ -24,12 +24,13 @@ class RegisterPopup extends Component {
         if (email.length !== 0 && password.length !== 0) {
             //check password
             if (password === repeatPass) {
-                if (password.length > 6 && password.length < 14) {
+                if (password.length >= 6 && password.length < 14) {
                     this.setState({ error: '' })
                     //password confirm
                     console.log('%c password correct', 'color:green');
                 } else {
                     this.setState({ error: 'รหัสผ่านควรมีความยาวระหว่าง 6 ถึง 14 ตัวอักษร' })
+                    console.log('รหัสผ่านควรมีความยาวระหว่าง 6 ถึง 14 ตัวอักษร');
                 }
                 // check email and username
                 database.ref('/users/').once('value', snapshot => {
@@ -40,7 +41,7 @@ class RegisterPopup extends Component {
                         }
                     })
                 })
-                if (this.state.error) {
+                if (!this.state.error) {
                     console.log('%c email, username correct', 'color:green');
                     // everything pass
 
